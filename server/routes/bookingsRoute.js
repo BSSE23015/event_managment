83e701adcfd7cc47e5621b5b28fed593edb650e9
Hassan Abid 15 <bsse23015@itu.edu.pk>
2026-03-24 11:04:20 +0500

    Booking route and booking controller added

--- server/routes/bookingsRoute.js
+++ server/routes/bookingsRoute.js
@@ -1,4 +1,17 @@
 import express from "express";
+import {
+  sendBookingOTP,
+  bookEvent,
+  confirmBooking,
+  getMyBookings,
+  cancelBooking,
+} from "../controllers/bookingController.js";
 const bookingRouter = express.Router();
+
+bookingRouter.post("/send-otp", protect, sendBookingOTP);
+bookingRouter.post("/", protect, bookEvent);
+bookingRouter.put("/:id/confirm", protect, admin, confirmBooking);
+bookingRouter.get("/my", protect, getMyBookings);
+bookingRouter.delete("/:id", protect, cancelBooking);
 
 export default bookingRouter;
